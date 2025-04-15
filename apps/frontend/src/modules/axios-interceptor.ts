46d87c4d9fc8ed1120e743d35a64c10adf3f376a
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-15 18:36:49 +0200

    feat: update logic for refresh token renewal.

--- apps/frontend/src/modules/axios-interceptor.ts
+++ apps/frontend/src/modules/axios-interceptor.ts
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import type { JwtResponse } from '@/types/modules/authentication.types.ts'
 
 axios.defaults.baseURL = import.meta.env.VITE_SERVER_URL
 axios.interceptors.request.use(
@@ -18,18 +19,25 @@ axios.interceptors.response.use(
   (response) => response,
   async (error) => {
     const originalRequest = error.config
+    const currentRefreshToken = localStorage.getItem('refresh_token')
 
-    if (error.response && error.response.status === 401 && !originalRequest._retry) {
+    if (
+      currentRefreshToken &&
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true
 
       try {
-        const refreshToken = localStorage.getItem('refresh_token')
-        const res = await axios.post('/auth/refresh', { refreshToken })
+        const res = await axios.post('/auth/refresh', { refreshToken: currentRefreshToken })
 
-        const newAccessToken = res.data.accessToken
-        localStorage.setItem('user_access_token', newAccessToken)
-        axios.defaults.headers.common['Authorization'] = `Bearer ${newAccessToken}`
-        originalRequest.headers['Authorization'] = `Bearer ${newAccessToken}`
+        const { accessToken, refreshToken } = res.data as JwtResponse
+
+        localStorage.setItem('user_access_token', accessToken)
+        localStorage.setItem('refresh_token', refreshToken)
+        axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`
+        originalRequest.headers['Authorization'] = `Bearer ${accessToken}`
 
         return axios(originalRequest)
       } catch (refreshError) {
