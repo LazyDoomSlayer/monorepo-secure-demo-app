7a5cb231945a6d260de9bffc48ac54d59591e1db
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-04-15 19:07:12 +0200

    feat: enable cors for development servers.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -11,7 +11,13 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
+  app.enableCors({
+    origin: ['http://localhost:4173', 'http://localhost:5173'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
   logger.log(`Application is listening on Port ${process.env.PORT ?? 3000}.`);
 }
+
 bootstrap();
