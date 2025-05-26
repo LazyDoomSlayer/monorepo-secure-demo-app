3308019ce65cf3a105a5d055781b8b58b1a59c75
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-05-26 16:24:22 +0200

    feat(ci): update ci settings for backend.

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -15,11 +15,7 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformInterceptor());
 
   app.enableCors({
-    origin: [
-      'http://localhost:4173',
-      'http://localhost:5173',
-      'https://monorepo-secure-demo-app.lazydoomslayer.dev/',
-    ],
+    origin: process.env.PORT?.split(',') || [],
     credentials: true,
   });
 
